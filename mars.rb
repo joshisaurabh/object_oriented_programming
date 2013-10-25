5dab04dcfa12a9f50249f2389dd7c1ec4d29df69
joshisaurabh <reachme15@yahoo.com>
2013-10-25 09:50:47 -0400

    mars rover -exercise 3

---
+++ mars.rb
@@ -0,0 +1,15 @@
+class Rover
+	attr_accessor :xaxis
+	attr_accessor :yaxis
+	attr_accessor :direction
+
+
+	def initialize(xaxis,yaxis,direction)
+
+		@xaxis = xaxis
+		@yaxis = yaxis
+		@direction = direction
+
+	end
+	
+end
